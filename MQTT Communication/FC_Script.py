4b1a53346ffa5924efbae7e8cbee0374352f8d5c
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2020-12-25 10:04:59 +0530

    Pi and FC Scripts Added

---
+++ MQTT Communication/FC_Script.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+data = "test"
+
+def on_message(client, userdata, message):
+    global data
+    data = message.payload.decode('utf-8')
+    print(data)
+    time.sleep(2)
+    
+client = mqtt.Client("Raspberry-Pi")
+broker = '15.206.122.229'
+client.connect(broker)
+
+client.subscribe('FC', qos = 2)
+
+while(data != "Done"):    
+    client.on_message = on_message
+    client.loop()
+    client.publish('Pi', "Location Reached", qos = 2)
+    
+print()
---
+++ MQTT Communication/Pi_Script.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+data = "test"
+data2 = "test"
+
+def on_message(client, userdata, message):
+    global data
+    data = message.payload.decode('utf-8')
+    print(data)
+    time.sleep(2)
+    
+client = mqtt.Client("Raspberry-Pi")
+broker = '15.206.122.229'
+client.connect(broker)
+
+#client.publish('FC', "hello", qos = 2)
+client.subscribe('Pi', qos = 2)
+
+while(1):    
+    client.on_message = on_message
+    client.loop()
+    if data == "Location Reached" and data2 != data:
+        take_sample()
+        take_readings()
+        client.publish('FC', "Done", qos = 2)
+        break    
+    data2 = data
+    
+    
+def take_sample():
+    time.sleep(2)
+    print("Sample Collected")
+def take_readings():
+    time.sleep(2)
+    print("Readings taken")
