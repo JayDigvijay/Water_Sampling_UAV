9257aabaa936ea1c42609a0d38cad6b849715970
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2020-12-25 11:48:53 +0530

    Modified Script

--- MQTT Communication/FC_Script.py
+++ MQTT Communication/FC_Script.py
@@ -9,15 +9,16 @@ def on_message(client, userdata, message):
     print(data)
     time.sleep(2)
     
-client = mqtt.Client("Raspberry-Pi")
+client = mqtt.Client("Flight-Controller")
 broker = '15.206.122.229'
 client.connect(broker)
 
 client.subscribe('FC', qos = 2)
 
-while(data != "Done"):    
+while(data != "Done"):
+    #print("started")    
     client.on_message = on_message
     client.loop()
     client.publish('Pi', "Location Reached", qos = 2)
     
-print()
+print("Finished")
