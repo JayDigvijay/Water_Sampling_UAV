5c07ace55a291760a7ef7abc195064fe94ec905f
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2020-12-27 14:15:08 +0530

    Working Communication

--- MQTT Communication/FC_Script.py
+++ MQTT Communication/FC_Script.py
@@ -10,16 +10,18 @@ def on_message(client, userdata, message):
     #time.sleep(2)
     
 client = mqtt.Client("Flight-Controller")
-broker = '15.206.122.229'
+broker = '13.232.250.241'
 client.connect(broker)
-
+#client.publish('Pi', "Location Reached", qos = 2)
+client.loop()
 client.subscribe('FC', qos = 2)
 
 while(1):
     #print("started")
-    if data == "Done":
+    if data == "Measurement Done":
         break    
     client.publish('Pi', "Location Reached", qos = 2)
+    time.sleep(3)
     client.on_message = on_message
     client.loop()
     
--- MQTT Communication/Pi_Script.py
+++ MQTT Communication/Pi_Script.py
@@ -9,30 +9,31 @@ def on_message(client, userdata, message):
     data = message.payload.decode('utf-8')
     print(data)
     #time.sleep(2)
+    
+client = mqtt.Client("Flight-Controller")
+broker = '13.232.250.241'
+client.connect(broker)
+
+client.subscribe('Pi', qos = 2)
+
 def take_sample():
     time.sleep(2)
     print("Sample Collected")
 def take_readings():
     time.sleep(2)
     print("Readings taken")
-      
-client = mqtt.Client("Raspberry-Pi")
-broker = '15.206.122.229'
-client.connect(broker)
-
-client.subscribe('FC', qos = 2)
-client.subscribe('Pi', qos = 2)
 
 while(1):
-	if data == "Location Reached" and data2 != data:
-		client.loop()
-		data2 = data
-		take_sample()
-		take_readings()
-        client.publish('FC', "Done", qos = 2)
-		break
-	client.on_message = on_message
-	client.loop()    
+    if data == "Location Reached" and data2 == "test":
+        data2 = data
+        take_sample()
+        take_readings()
+        client.publish('FC', "Measurement Done", qos = 1)
+        client.loop(3)
+        client.on_message = on_message
+        break
+    client.on_message = on_message
+    client.loop(0.1)    
 
     
     
