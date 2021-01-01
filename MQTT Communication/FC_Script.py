0cc16cae9b585620831ee77c9804432867699c12
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2021-01-01 19:01:47 +0530

    Final Changes

--- MQTT Communication/FC_Script.py
+++ MQTT Communication/FC_Script.py
@@ -7,24 +7,25 @@ def on_message(client, userdata, message):
     global data
     data = message.payload.decode('utf-8')
     print(data)
+    if data == "Measurement Done":
+        print("Finished")
     #time.sleep(2)
-    
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code " + str(rc))
+	client.subscribe("FC")
+
+  
 client = mqtt.Client("Flight-Controller")
 broker = 'broker.hivemq.com'
 client.connect(broker, 1883, 8000)
-#client.publish('Pi', "Location Reached", qos = 2)
-client.loop()
-client.subscribe('FC', qos = 2)
+client.on_connect = on_connect
+
 client.publish('Pi', "Location Reached", qos = 1)
 
-while(1):
-    #print("started")
-    if data == "Measurement Done":
-        break    
-    
-    #time.sleep(10)
-    client.on_message = on_message
-    client.loop_forever()
+
+#time.sleep(10)
+client.on_message = on_message
+client.loop_forever()
     
     
-print("Finished")
--- MQTT Communication/Pi_Script.py
+++ MQTT Communication/Pi_Script.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
-import json
+
 data = "test"
 data2 = "test"
 
@@ -8,13 +8,20 @@ def on_message(client, userdata, message):
     global data
     data = message.payload.decode('utf-8')
     print(data)
+    if data == "Location Reached" and data2 == "test":
+        data2 = data
+        take_sample()
+        take_readings()
+        client.publish('FC', "Measurement Done", qos = 1)
     #time.sleep(2)
-    
-client = mqtt.Client("Flight-Controller")
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+	client.subscribe("Pi")
+   
+client = mqtt.Client("Pi")
 broker = 'broker.hivemq.com'
 client.connect(broker)
-
-client.subscribe('Pi', qos = 2)
 
 def take_sample():
     time.sleep(2)
@@ -23,17 +30,9 @@ def take_readings():
     time.sleep(2)
     print("Readings taken")
 
-while(1):
-    if data == "Location Reached" and data2 == "test":
-        data2 = data
-        take_sample()
-        take_readings()
-        client.publish('FC', "Measurement Done", qos = 1)
-        client.on_message = on_message
-        client.loop_forever
-        break
-    client.on_message = on_message
-    client.loop(0.1)    
+client.on_connect = on_connect
+client.on_message = on_message
+client.loop_forever()  
 
     
     
